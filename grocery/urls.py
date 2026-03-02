e8d3c9b3f2f2652a640ab45126d0eb8f1a1627b0
unnati sangraula <unnatisangraula@gmail.com>
2026-03-02 13:58:42 +0545

    update grocerry urls

--- grocery/urls.py
+++ grocery/urls.py
@@ -6,4 +6,5 @@ app_name = 'grocery'
 urlpatterns = [
     path('', views.index, name='index'),
     path('toggle/<int:item_id>/', views.toggle_completed, name='toggle'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete'),
 ]
