7e599936aad69feb65503c13010f7b4714625b27
unnati sangraula <unnatisangraula@gmail.com>
2026-03-02 13:27:59 +0545

    models

--- grocery/models.py
+++ grocery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class GroceryItem(models.Model):
+    name = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created_at']
