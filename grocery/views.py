6c0aed6d99da84b02a5f5dc4eb47e755255622ac
unnati sangraula <unnatisangraula@gmail.com>
2026-03-02 13:34:33 +0545

    update views

--- grocery/views.py
+++ grocery/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import GroceryItem
+
 
 def index(request):
     """Display all grocery items"""
-    return render(request, 'grocery/index.html')
+    items = GroceryItem.objects.all()
+    context = {
+        'items': items,
+    }
+    return render(request, 'grocery/index.html', context)
